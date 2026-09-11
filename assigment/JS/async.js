40e4cc269f538e3b0fd884fa4052ea3e93b4d52d
Nadegeida <idatchatchau@gmail.com>
2026-09-11 04:53:50 +0100

    Delete assigment directory

--- assigment/JS/async.js
+++
@@ -1,5 +0,0 @@
-alert(document.getElementById("p1").innerHTML)
-function changeText() {
-    document.getElementById("p1").innerText="Hello world! The defer attribute allowed this script to load after the HTML element was parsed.";
-}
-changeText;
